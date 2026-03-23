a79ad5000ed1a6fec0e320a0333bb62cfefd95ec
amazing678 <94849875+amazing678@users.noreply.github.com>
2026-03-23 11:09:14 +0800

    Update ScalingLabelBehavior.cs

--- UI/Windows/Chest Window/ScalingLabelBehavior.cs
+++ UI/Windows/Chest Window/ScalingLabelBehavior.cs
@@ -7,15 +7,42 @@ namespace OctoberStudio.UI
 {
     public class ScalingLabelBehavior : MonoBehaviour
     {
+        [Tooltip("填 1 代表 P1，填 2 代表 P2")]
+        public int playerId = 1;
+        [Tooltip("如果这个UI是用来显示击杀数的，请勾选这个开关")]
+        public bool isKillCounter = false;
+
         [SerializeField] protected TMP_Text label;
         [SerializeField] protected Image icon;
         [SerializeField] AligmentType aligment;
 
         private float spacing;
+        private int lastValue = -1;
 
         private void Awake()
         {
             spacing = label.rectTransform.anchoredPosition.x - label.rectTransform.sizeDelta.x / 2 - icon.rectTransform.anchoredPosition.x - icon.rectTransform.sizeDelta.x / 2;
+        }
+
+        private void Update()
+        {
+            // 如果是击杀数 UI，就在这里读取对应玩家的击杀数据
+            if (isKillCounter)
+            {
+                var player = (playerId == 1) ? PlayerBehavior.Player1 : PlayerBehavior.Player2;
+                if (player != null && player.kills != lastValue)
+                {
+                    lastValue = player.kills;
+                    SetAmount(lastValue);
+                    transform.localScale = Vector3.one * 1.3f; // 击杀跳动动画
+                }
+            }
+
+            // 平滑恢复大小动画
+            if (transform.localScale.x > 1.01f)
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, Time.unscaledDeltaTime * 10f);
+            }
         }
 
         public void SetAmount(int amount)
